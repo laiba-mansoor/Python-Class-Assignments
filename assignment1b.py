6445c0c37175d127aecffb9333df4f4c8331e04d
laiba-mansoor <laiba.maryam1191@gmail.com>
2019-11-06 19:31:58 +0500

    Assignment

---
+++ assignment1b.py
@@ -0,0 +1,3 @@
+a=int(input('enter radius of circle '))
+area=3.14*(a)**2
+print('area of circle is ',area)
---
+++ assignment1d.py
@@ -0,0 +1,4 @@
+a=int(input('enter radius of circle '))
+area=3.142*(a)**2
+print('area of circle is ',area)
+
